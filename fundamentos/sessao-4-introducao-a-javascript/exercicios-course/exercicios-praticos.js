a8e7e04239abdce07758550582fccb19d5eb0ce7
Dhiego Balthazar <dhiegobalthazar@gmail.com>
2022-11-26 00:15:20 -0300

    imprime o maior

--- fundamentos/sessao-4-introducao-a-javascript/exercicios-course/exercicios-praticos.js
+++ fundamentos/sessao-4-introducao-a-javascript/exercicios-course/exercicios-praticos.js
@@ -1,15 +1,20 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-let soma = 0;
+// let soma = 0;
+let maior = 0;
 for(let index = 0; index < numbers.length; index++){
+    let value = numbers[index];
     // console.log(numbers[index]);
-    soma += numbers[index];
+    // soma += numbers[index];
+    if(maior < value){
+        maior = value;
+    }
 }
-
+console.log(maior);
 let media = soma/numbers.length;
 // console.log(media);
-if (media>20) {
-    console.log("Valor maior que vinte!!!");
-}else{
-    console.log("valor menor ou igual a vinte!!!!!");
-}
+// if (media>20) {
+//     console.log("Valor maior que vinte!!!");
+// }else{
+//     console.log("valor menor ou igual a vinte!!!!!");
+// }
