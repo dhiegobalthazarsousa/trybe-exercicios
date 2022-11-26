d8acaa2dc5375f5b05138d9d279e3ad279822d41
Dhiego Balthazar <dhiegobalthazar@gmail.com>
2022-11-25 23:41:18 -0300

    inicio

--- fundamentos/sessao-4-introducao-a-javascript/exercicio-3.js
+++
@@ -1,9 +0,0 @@
-const nota = 100;
-
-if(nota < 60){
-    console.log("você foi reprovado!");
-}else if(nota >= 80){
-    console.log("Parabéns, você foi aprovado!");
-}else{
-    console.log("Você está na fila de espera!");
-}
