9635bc9c32eaa5ebcd706daff0abeac586d288f1
Dhiego Balthazar <dhiegobalthazar@gmail.com>
2022-11-26 00:07:57 -0300

    calcula media

--- fundamentos/sessao-4-introducao-a-javascript/exercicios-course/exercicios-praticos.js
+++ fundamentos/sessao-4-introducao-a-javascript/exercicios-course/exercicios-praticos.js
@@ -1,6 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
+let soma = 0;
 for(let index = 0; index < numbers.length; index++){
-    console.log(numbers[index]);
+    // console.log(numbers[index]);
+    soma += numbers[index];
 }
+
+let media = soma/numbers.length;
+console.log(media);
 
