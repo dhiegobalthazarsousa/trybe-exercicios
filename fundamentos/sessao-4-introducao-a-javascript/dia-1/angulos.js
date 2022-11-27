444d73e929aeba016c552915219f64f165ae94f8
Dhiego Balthazar <dhiegobalthazar@gmail.com>
2022-11-27 11:31:36 -0300

    finaliza triangulo

---
+++ fundamentos/sessao-4-introducao-a-javascript/dia-1/angulos.js
@@ -0,0 +1,9 @@
+const angulo1 = 180;
+const angulo2 = 1;
+const angulo3 = -1;
+let soma = angulo1 + angulo2 + angulo3;
+if((angulo1 > 0 && angulo2 > 0 && angulo3 > 0) && soma === 180){
+    console.log('True');
+}else{
+    console.log('False');
+}
