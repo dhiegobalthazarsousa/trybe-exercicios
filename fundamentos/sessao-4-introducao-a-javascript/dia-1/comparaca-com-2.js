4e9c8d086aaa54bded5617b517debc4f2d5cb6fe
Dhiego Balthazar <dhiegobalthazar@gmail.com>
2022-11-27 09:01:09 -0300

    comparacao 2 numeros

---
+++ fundamentos/sessao-4-introducao-a-javascript/dia-1/comparaca-com-2.js
@@ -0,0 +1,10 @@
+const numero1 = 30;
+const numero2 = 50;
+
+if(numero1 > numero2){
+    console.log("O 1º número é o maior");
+}else if (numero2 > numero1){
+    console.log("O 2º número é o maior");
+}else{
+    console.log("Os dois números são iguais");
+}
---
+++ fundamentos/sessao-4-introducao-a-javascript/dia-1/operandos.js
@@ -0,0 +1,8 @@
+let a = 3;
+let b = 5;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
