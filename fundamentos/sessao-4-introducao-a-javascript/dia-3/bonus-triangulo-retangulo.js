98f18b3422dd09803fee6696a4239df966b99aea
Dhiego Balthazar <dhiegobalthazar@gmail.com>
2022-11-28 19:35:53 -0300

    realiza exercicio bonus triangulo retangulo

---
+++ fundamentos/sessao-4-introducao-a-javascript/dia-3/bonus-triangulo-retangulo.js
@@ -0,0 +1,9 @@
+const numero = 5;
+let linha = '';
+for(let contLinha = 1; contLinha <= numero; contLinha++){
+    for(let contColuna = 0; contColuna < contLinha; contColuna++){
+        linha = linha + '*';
+    }
+    console.log(linha);
+    linha = '';
+}
