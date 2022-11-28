bfcd3dddee51fdf48ff4a39bfee8f2c97645e9d2
Dhiego Balthazar <dhiegobalthazar@gmail.com>
2022-11-27 21:04:49 -0300

    resolve valor do lucro

---
+++ fundamentos/sessao-4-introducao-a-javascript/dia-1/ha-1-par-entre-nos-1.js
@@ -0,0 +1,8 @@
+const numero1 = 39;
+const numero2 = 48;
+const numero3 = 45;
+if(numero1 % 2 === 0 || numero2 % 2 === 0 || numero3 % 2 === 0){
+    console.log("Há um par entre nós!");
+}else{
+    console.log("não há nenhum par entre nós!");
+}
---
+++ fundamentos/sessao-4-introducao-a-javascript/dia-1/valor-do-lucro.js
@@ -0,0 +1,7 @@
+const costValue = 100.00;
+const sellValue = 200.00;
+
+let totalCost = costValue * 1.2;
+let profitPerUnity = sellValue - totalCost;
+
+console.log(profitPerUnity * 1000);
