567b9cb7971286e5eada1a0890c61ac76ea799d3
Dhiego Balthazar <dhiegobalthazar@gmail.com>
2022-11-30 22:56:09 -0300

    realiza o exercicio  ambos recorrente

--- fundamentos/sessao-4-introducao-a-javascript/dia-4/boasVindas.js
+++ fundamentos/sessao-4-introducao-a-javascript/dia-4/boasVindas.js
@@ -4,14 +4,30 @@ let info = {
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   };
 
+  let info2 = {
+    personagem: 'Tio Patinha',
+    origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim',
+  };
+
 //   console.log("Bem vinda " + info.personagem);
 info['recorrente'] = 'SIM';
+
+for(let key in info){
+    if(key !== 'recorrente'){
+        console.log(info[key] + ' e ' + info2[key]);
+    }
+}
+if(info['recorrente'].toLowerCase() === 'sim' && info2['recorrente'].toLowerCase() === 'sim'){
+    console.log("Ambos recorrentes");
+}
 // console.log(info);
 
 // for(let key in info){
 //     console.log(key);
 // }
 
-for(let key in info){
-    console.log(info[key]);
-}
+// for(let key in info){
+//     console.log(info[key]);
+// }
