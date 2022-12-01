a5f35a7875bdeee6dd79a3c7395fd406cb0cb8a9
Dhiego Balthazar <dhiegobalthazar@gmail.com>
2022-12-01 08:17:13 -0300

    finaliza exercicio ending word

--- fundamentos/sessao-4-introducao-a-javascript/dia-4/funcoes.js
+++ fundamentos/sessao-4-introducao-a-javascript/dia-4/funcoes.js
@@ -127,3 +127,28 @@ function sumSequency(number){
 
 let number = 5;
 console.log(sumSequency(number));
+
+// Crie uma função que receba uma string word e outra string ending. Verifique se a string ending é o final da string word. Considere que a string ending sempre será menor que a string word.
+
+// Valor de teste: 'trybe' e 'be'
+
+// Valor esperado no retorno da função: true
+
+// verificaFimPalavra('trybe', 'be');
+
+// Retorno esperado: true
+// verificaFimPalavra('joaofernando', 'fernan');
+
+// Retorno esperado: false
+
+function endingVerify(word, ending){
+    if(word.endsWith(ending)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+let word = 'trybe';
+let ending = 'ybe';
+console.log(endingVerify(word, ending));
