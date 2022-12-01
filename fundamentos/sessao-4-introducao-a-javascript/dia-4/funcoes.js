33e019f7d62418a8407188efa1004a456f8af30e
Dhiego Balthazar <dhiegobalthazar@gmail.com>
2022-12-01 08:10:26 -0300

    realiza exercicio de contagem de numeros e soma sequencia

--- fundamentos/sessao-4-introducao-a-javascript/dia-4/funcoes.js
+++ fundamentos/sessao-4-introducao-a-javascript/dia-4/funcoes.js
@@ -82,3 +82,48 @@ function mostChar(array){
 
 let array = ['José', 'Lucas', 'Nádia', 'Fernanda', 'CairoCastroBolivar', 'Joana'];
 console.log(mostChar(array));
+
+// Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+
+// Array de teste: [2, 3, 2, 5, 8, 2, 3];.
+
+// Valor esperado no retorno da função: 2.
+
+
+function countRepetition(array){
+    let indexMoreRepeated = 0;
+    let quantityTimes = 0;
+    for(let keyArray in array){
+        let count = 0;
+        for(let keyComparison in array){
+            if(array[keyArray] === array[keyComparison]){
+                count++;
+            }
+        }
+        if(count > quantityTimes){
+            indexMoreRepeated = keyArray;
+            quantityTimes = count;
+        }
+    }
+    return array[indexMoreRepeated];
+}
+
+let array = [2, 3, 2, 3, 3, 2, 3];
+console.log(countRepetition(array));
+
+// Crie uma função que receba um número natural (número inteiro não negativo) N e retorne o somatório de todos os números de 1 até N.
+
+// Valor de teste: N = 5.
+
+// Valor esperado no retorno da função: 1+2+3+4+5 = 15.
+
+function sumSequency(number){
+    let sum = 0;
+    for(let iteration = 1; iteration <= number; iteration++){
+        sum += iteration;
+    }
+    return sum;
+}
+
+let number = 5;
+console.log(sumSequency(number));
