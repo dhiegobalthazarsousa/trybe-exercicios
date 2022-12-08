b4f5cefa0472ecff9ee53e264f68897a0c90d472
Dhiego Balthazar <dhiegobalthazar@gmail.com>
2022-12-07 23:31:52 -0300

    realiza exercicios 2

--- fundamentos/sessao-5-DOM-eventos-web-storage/dia-1-javascript-seletores/script.js
+++ fundamentos/sessao-5-DOM-eventos-web-storage/dia-1-javascript-seletores/script.js
@@ -1,2 +1,6 @@
 const paragraph = document.querySelectorAll(".center-content p")[1];
 paragraph.innerText = "Daqui a dois anos eu me vejo trabalhando como dev em uma multinacional, fora do país";
+paragraph.append(" e feliz");
+
+const mainContent = document.querySelector(".main-content");
+mainContent.style.backgroundColor = "rgb(76,164,109)";
