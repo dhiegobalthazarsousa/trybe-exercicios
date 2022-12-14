2a558df72dcd06ee70a5c22461fba62c9022f049
Dhiego Balthazar <dhiegobalthazar@gmail.com>
2022-12-13 22:47:54 -0300

    arrumando node

---
+++ fundamentos/sessao-4-introducao-a-javascript/dia-5/teste-aula.js
@@ -0,0 +1,28 @@
+// //template literals
+// let nome = 'Dhiego';
+// let sobrenome = 'Balthazar';
+// console.log(`Meu nome é ${nome} ${sobrenome}!`);
+
+// let a = 10;
+// let b = 10;
+// let porcentagem = 0.10;
+
+// let soma = porcentagem => {
+//     let soma = a + b;
+//     return soma * (1+porcentagem);
+// }
+
+// console.log(soma(porcentagem));
+
+// let frase = 'eu sou gostoso';
+// console.log(frase.split(' ')[0]);
+
+let numero = 10;
+let bool = true;
+
+function func(){
+
+    return numero;
+}
+
+console.log(func());
